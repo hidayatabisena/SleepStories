c0ef306ad0525fff1444daea56ccddc1a8fc02c1
Hidayat Abisena <plegmasena@gmail.com>
2022-09-25 19:36:13 +0700

    Updating launch screen background image and bg color

--- SleepStories/App/SleepStoriesApp.swift
+++ SleepStories/App/SleepStoriesApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SleepStoriesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // ContentView()
+            OnboardingView()
         }
     }
 }
--- SleepStories/View/OnboardingView.swift
+++ SleepStories/View/OnboardingView.swift
@@ -13,70 +13,72 @@ struct OnboardingView: View {
     
     // MARK: - BODY
     var body: some View {
-        VStack {
-            Text("Welcome to Sleep Stories")
-                .foregroundColor(.white)
-                .font(.system(size: 40, design: .rounded))
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
+        ZStack {
+            Image("bg-iphone")
+                .resizable()
+                .ignoresSafeArea(.all, edges: .all)
             
-            Text("Explore the app. Find some peace of mind to prepare for meditation")
-                .foregroundColor(.white)
-                .lineLimit(2)
-                .font(.headline)
-                .multilineTextAlignment(.center)
-                .padding(.top, 20)
-            
-            ZStack {
-                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
-                
-                Image("character-1")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                    .offset(y: isAnimating ? 35 : -35)
-                    .animation(
-                        Animation
-                            .easeInOut(duration: 4)
-                            .repeatForever()
-                        , value: isAnimating
-                    )
-                
-            } //: ZSTACK
-            .padding(.top, 20)
-            
-            Spacer()
-            
-            Button(action: {
-                print("Button tapped")
-            }, label: {
-                Image(systemName: "sleep.circle.fill")
-                    .imageScale(.large)
-                
-                Text("Get started".uppercased())
-                    .font(.system(.title3, design: .rounded))
+            VStack {
+                Text("Welcome to Sleep Stories")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, design: .rounded))
                     .fontWeight(.bold)
-            }) //: BUTTON
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .controlSize(.large)
-        } //: VSTACK
-        .padding()
-        .onAppear(perform: {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-                isAnimating = true
-            })
+                    .multilineTextAlignment(.center)
+                
+                Text("Explore the app. Find some peace of mind to prepare for meditation.")
+                    .foregroundColor(.white)
+                    .lineLimit(2)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 20)
+                
+                ZStack {
+                    CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .offset(y: isAnimating ? 35 : -35)
+                        .animation(
+                            Animation
+                                .easeInOut(duration: 4)
+                                .repeatForever()
+                            , value: isAnimating
+                        )
+                    
+                } //: ZSTACK
+                .padding(.top, 20)
+                
+                Spacer()
+                
+                Button(action: {
+                    print("Button tapped")
+                }, label: {
+                    Image(systemName: "sleep.circle.fill")
+                        .imageScale(.large)
+                    
+                    Text("Get started".uppercased())
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                }) //: BUTTON
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+            } //: VSTACK
+            .padding()
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    isAnimating = true
+                })
         })
+        } //: ZSTACK
     }
 }
 
 // MARK: - PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        ZStack {
-            Color("ColorBlue")
-                .ignoresSafeArea(.all, edges: .all)
-            OnboardingView()
-        }
+        OnboardingView()
     }
 }
