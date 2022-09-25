49a4a1d1c606b9102da84d8ace65fcd1faa3eb9d
Hidayat Abisena <plegmasena@gmail.com>
2022-09-25 17:03:59 +0700

    Project Setup

---
+++ SleepStories/App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SleepStories
+//
+//  Created by Hidayat Abisena on 25/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SleepStories/App/SleepStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepStoriesApp.swift
+//  SleepStories
+//
+//  Created by Hidayat Abisena on 25/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
