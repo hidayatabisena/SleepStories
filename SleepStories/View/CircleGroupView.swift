68ddb73150c88a1dac4078e49a4556e5f77213d3
Hidayat Abisena <plegmasena@gmail.com>
2022-09-25 17:45:59 +0700

    Setup Onboarding Screen View

---
+++ SleepStories/View/CircleGroupView.swift
@@ -0,0 +1,47 @@
+//
+//  CircleGroupView.swift
+//  SleepStories
+//
+//  Created by Hidayat Abisena on 25/09/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    // MARK: - PROPERTIES
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    @State private var isAnimating: Bool = false
+    
+    // MARK: - BODY
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+        .blur(radius: isAnimating ? 0 : 10)
+        .opacity(isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1 : 0.5)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear(perform: {
+            isAnimating = true
+        })
+    }
+}
+
+// MARK: - PREVIEW
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
